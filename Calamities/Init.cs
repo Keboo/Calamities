9402073de27351625a0d4273b36a7c6467016527
Kevin Bost <kitokeboo@gmail.com>
2021-08-19 21:46:20 -0700

    Adding module initializer that changes string constant

---
+++ Calamities/Init.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+class Init
+{
+    [ModuleInitializer]
+    internal static void ModuleInitializer()
+    {
+        var v = new(); //Ensure var is initialized
+        const string name = "Bob";
+        const string newName = "Joe";
+
+        unsafe
+        {
+            fixed (char* ptr = name)
+            {
+                for (int i = 0; i < name.Length; i++)
+                {
+                    ptr[i] = newName[i];
+                }
+            }
+        }
+    }
+}
--- Calamities/var.cs
+++ Calamities/var.cs
@@ -1,14 +1,13 @@
-﻿namespace Calamities
+﻿
+class var
 {
-    class var
+    private string _originalValue;
+
+    public static implicit operator var(string str)
     {
-        private string _originalValue;
-
-        public static implicit operator var(string str)
-        {
-            return new var() { _originalValue = str };
-        }
-
-        public override string ToString() => "Bob";
+        return new var() { _originalValue = str };
     }
+
+    public override string ToString() => "Bob";
 }
+
