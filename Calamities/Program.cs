89e87ad7f6d5373dcf95ab0eedb453852dbc4d69
Kevin Bost <kitokeboo@gmail.com>
2021-08-19 21:12:51 -0700

    Add project files.

---
+++ Calamities/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calamities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your birthday");
+            String birthday = Console.ReadLine();
+
+            if (DateTime.TryParse(birthday, out DateTime birthdateDateTime))
+            {
+                TimeSpan age = DateTime.Now - birthdateDateTime;
+                Console.WriteLine($"{name} you are {age.TotalDays} days old");
+            }
+            else
+            {
+                Console.Error.WriteLine("Failed to parse birthday");
+            }
+        }
+    }
+}
